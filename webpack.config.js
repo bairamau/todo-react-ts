4300c3afe6befb697c01939683ccff63e2253afd
zaur <bairamau.z@gmail.com>
2019-06-18 16:53:04 +0300

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.tsx",
   output: {
@@ -23,12 +25,27 @@ module.exports = {
     ]
   },
 
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
+
   // When importing a module whose path matches one of the following, just
   // assume a corresponding global variable exists and use that instead.
   // This is important because it allows us to avoid bundling all of our
   // dependencies, which allows browsers to cache those libraries between builds.
+  // externals: {
+  //   react: "React",
+  //   "react-dom": "ReactDOM"
+  // },
+
+  devServer: {
+    port: 3000,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  },
+
   externals: {
-    react: "React",
+    "react": "React",
     "react-dom": "ReactDOM"
   }
 };
